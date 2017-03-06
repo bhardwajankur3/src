041aff8d620a332898393c9509310e5ca0835322
joeljmj26 <joeljmj26@gmail.com>
2017-03-06 13:04:49 -0700

    Correcting errors in file

--- global/Descriptor.java
+++ global/Descriptor.java
@@ -4,12 +4,14 @@ import static java.lang.Math.sqrt;
 
 public class Descriptor {
     short[] value = new short[5];
-    value[0] = 0;
-    value[1] = 0;
-    value[2] = 0;
-    value[3] = 0;
-    value[4] = 0;
-    
+    Descriptor()
+    {
+        value[0] = 0;
+        value[1] = 0;
+        value[2] = 0;
+        value[3] = 0;
+        value[4] = 0;
+    }
     void set(short value0, short value1, short value2, short value3, short value4) {
         value[0] = value0;
         value[1] = value1;
@@ -34,7 +36,7 @@ public class Descriptor {
     double distance (Descriptor desc) {
         int dist = 0;
         for(int i = 0; i < 5; i++){
-            dist = dist + ((this.value[i] - desc.value[i]) * (this.value[i] - desc.value[i]))
+            dist = dist + ((this.value[i] - desc.value[i]) * (this.value[i] - desc.value[i]));
         }
         return sqrt(dist);
     } 
